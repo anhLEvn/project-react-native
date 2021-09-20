bf8c184f63399c27102b959fc630d2c53e451522
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-20 11:42:03 +0200

    first commit

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+// tout module ou composant exporter par default sera importer de maniere suivnante 
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,7 +7,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
