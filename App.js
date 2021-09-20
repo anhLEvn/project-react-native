d44c7f0b9ded2a291d93b630e06fe57807c1aabb
LE Thi Van Anh <57898934+anhLEvn@users.noreply.github.com>
2021-09-20 13:54:04 +0200

    change app.js

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-// tout module ou composant exporter par default sera importer de maniere suivnante 
+// tout module ou composant exporter par defaut sera importer de la maniere suivante:
+// import nomDuModule from 'chemin vers le fifier ou est defini le module / la librairie'
+// tout module qui n'est pas exporter par defaut sera importer de la maniere suivante:
+// import {nomDuModule} from 'chemin vers le fifier ou est defini le module / la librairie'
 
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Test, { Bonjour, MyComponent, DireBonjour} from './components/Test'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Test />
+      <Bonjour />
+      <MyComponent arg1 = 'valeur de la propriete1' param ='valeur de paramettre'/>
+      <DireBonjour nom = 'toto'/>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
+// export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,4 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+// dans un fichier il peut avoir qu'un seul expot part defaut
+// si nous souhaitons creer plusieurs modules ou composants dans un fichier
+// mis a part le module exporter par defaut tout les autres module doivent etre exporter de la  maniere suivante:
+// export {module1, module2,...}
